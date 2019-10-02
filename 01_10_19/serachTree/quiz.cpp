3e2c4adc990ebcd6e543b82af6a84d9012f1afe0
Byeongryul <mapia56@naver.com>
2019-10-02 11:03:58 +0000

    search_Tree

--- 01_10_19/serachTree/quiz.cpp
+++ 01_10_19/serachTree/quiz.cpp
@@ -1,96 +1,10 @@
 #include "quiz.h"
 
-enum Option{
-    Search, Insert, Delete
-};
-
-void print_not_found(){
-    printf("존재하지 않습니다!\n");
-}
-
-void search_tree_all(Node **Root, Node *input, Option option, bool is_See){
-    Node *root = *Root, *parent = NULL;
-    //root가 없거나, 입력값이 현재 위치면 멈춤
-    while(root == NULL || root->data == input->data){
-        //부모노드를 설정함
-        parent = root;
-        //입력값을 찾기 위해 왼쪽으로 이동
-        if(root->data > input->data)      root = root->left;
-        //입력값을 찾기 위해 오른쪽으로 이동
-        else if(root->data < input->data) root = root->right;
-    }
-    if(root == NULL){
-        //모든 값이 존재하지 않음
-        if(parent == NULL){
-            switch(option){
-                case Search:
-                    //print_not_found();
-                    break;
-                case Insert:
-
-                    break;
-                case Delete:
-                    print_not_found();
-                    break;
-            }
-        }
-        //입력값이 존재하지 않음
-        else{
-            switch(option){
-                case Search:
-                    //print_not_found();
-                    break;
-                case Insert:
-                    break;
-                case Delete:
-                    print_not_found();
-                    break;
-            }
-        }
-    }
-    else{
-        //입력값이 root값
-        if(parent == NULL){
-            switch(option){
-                case Search:
-                    break;
-                case Insert:
-                    break;
-                case Delete:
-                    break;
-            }
-        }
-        //입력값이 가운대 존재함
-        else{
-            switch(option){
-                case Search:
-                    break;
-                case Insert:
-                    break;
-                case Delete:
-                    break;
-            }
-        }
-    }
-}
-
 element num_rand(){
     int range = NUM_RANGE_MAX - NUM_RANGE_MIN + 1;
     return rand() % range + NUM_RANGE_MIN;
 }
-
-void Insert_tree(Node **root){
-    element data = num_rand();
-
-    while(search_tree(*root, data) != NULL) data = num_rand();
-
-    printf("입력 값: %d\n", data);
-
-    Node *New = TreeNodeInit(data);
-    if(*root == NULL) *root = New;
-    else inserttree(*root, New);
-}
-
+  
 Node* search_tree(Node *root, element data){// 존재여부 확인
     if(root == NULL) return NULL;// 없으면 NULL 출력
     else if(root->data == data) return root;// 있다면 그 노드를 출력
